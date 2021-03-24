19e66c3565bc43c54835f94e2e3024ca5b8ff8a2
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-23 20:24:44 -0600

    edit API call to receive less information

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASEURL =
+	"https://randomuser.me/api/?inc=name,email,picture,phone?results=30";
+
 export default {
 	getUsers: function () {
-		return axios.get("https://randomuser.me/api/?results=30");
+		return axios.get(BASEURL);
 	},
 };
