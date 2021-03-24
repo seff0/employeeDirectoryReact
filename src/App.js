b8e1b168e308c5c9621abd18546a44675a66a57d
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-23 21:15:26 -0600

    move API call again from App.js to Wrapper.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
-import UserCard from "./components/UserCard";
-import axios from "axios";
 
-class App extends Component {
-	state = {
-		employees: [],
-	};
-
-	componentDidMount() {
-		axios
-			.get("https://randomuser.me/api/?inc=name,email,picture,phone&results=30")
-			.then((res) => {
-				this.setState({ employees: res.data.results });
-				console.log(res);
-			});
-	}
-
-	render() {
-		return (
-			<div>
-				<Wrapper>
-					{this.state.employees.map((employee, index) => (
-						<UserCard
-							key={index}
-							name={employee.name}
-							email={employee.email}
-							picture={employee.picture}
-							phone={employee.phone}
-						/>
-					))}
-				</Wrapper>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div>
+			<Wrapper></Wrapper>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import UserCard from "../UserCard";
 
-function Wrapper() {
-	return <div className="grid-container"></div>;
+class Wrapper extends Component {
+	state = {
+		employees: [],
+	};
+
+	componentDidMount() {
+		axios
+			.get("https://randomuser.me/api/?inc=name,email,picture,phone&results=30")
+			.then((res) => {
+				this.setState({ employees: res.data.results });
+				console.log(res);
+			});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row row-cols-3">
+					{this.state.employees.map((employee, index) => (
+						<UserCard
+							key={index}
+							name={employee.name}
+							email={employee.email}
+							picture={employee.picture}
+							phone={employee.phone}
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Wrapper;
